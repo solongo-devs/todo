b8bee2fcf8d9930c3d73a7093c9e9717b393c42f
Felix Klotzsche <felixklotzsche@gmail.com>
2016-01-17 14:18:00 +0100

    Test for content validation

--- app/models/todomo.rb
+++ app/models/todomo.rb
@@ -1,2 +1,3 @@
 class Todomo < ActiveRecord::Base
+  validates :content, presence: true
 end
--- spec/models/todomo_spec.rb
+++ spec/models/todomo_spec.rb
@@ -21,4 +21,12 @@ RSpec.describe Todomo, type: :model do
   it "should have done_time" do
     expect(@todomo).to respond_to :done_time
   end
+
+  describe "validation" do
+    it "should not be valid with empty content" do
+      todomo = Todomo.new
+      todomo.content = ""
+      expect(todomo).not_to be_valid
+    end
+  end
 end
