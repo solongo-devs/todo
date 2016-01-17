4bd78b4e13b667e80e421c57bd92a1797978149d
Felix Klotzsche <felixklotzsche@gmail.com>
2016-01-17 16:45:45 +0100

    Stuff deleted, new Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Todomo.create content: "Hi", title: "Bye"
+Todomo.create content: "Hi", title: "Bye", done_time: Time.now
