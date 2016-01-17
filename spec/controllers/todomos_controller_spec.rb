c68e48104817e5d7610cd0508f9bb9176bbac829
marcel <roeslerm@wh12.tu-dresden.de>
2016-01-17 16:45:03 +0100

    more controller tests for the responding jsonstring

--- spec/controllers/todomos_controller_spec.rb
+++ spec/controllers/todomos_controller_spec.rb
@@ -7,11 +7,32 @@ RSpec.describe TodomosController, type: :controller do
     expect(response.status).to eq 200
   end
 
-  it "should respond to index" do
+  it "should respond to index and has a title" do
     Todomo.create content: "hi", title: "heyho"
     get :index
     todomos = JSON.parse(response.body)
     expect(todomos[0]['title']).to eq "heyho"
+  end
+
+  it "should respond to index and has a content" do
+    Todomo.create content: "hi", title: "heyho"
+    get :index
+    todomos = JSON.parse(response.body)
+    expect(todomos[0]['content']).to eq "hi"
+  end
+
+  it "should respond to index and has a donw" do
+    Todomo.create content: "hi", title: "heyho"
+    get :index
+    todomos = JSON.parse(response.body)
+    expect(todomos[0]['done']).to eq false
+  end
+
+  it "should respond to index and has a title" do
+    Todomo.create content: "hi", title: "heyho"
+    get :index
+    todomos = JSON.parse(response.body)
+    expect(todomos[0]['done_time']).to eq nil
   end
 
   after :each do
