32eacd0ab1005c66577fe14cf701eb11e6bd005b
Felix Klotzsche <felixklotzsche@gmail.com>
2016-01-17 16:36:41 +0100

    Todomo Controller gebaut und getestet

--- app/controllers/todomos_controller.rb
+++ app/controllers/todomos_controller.rb
@@ -1,2 +1,6 @@
 class TodomosController < ApplicationController
+  def index
+    todomos = Todomo.all
+    render json: todomos
+  end
 end
--- spec/controllers/todomos_controller_spec.rb
+++ spec/controllers/todomos_controller_spec.rb
@@ -7,4 +7,15 @@ RSpec.describe TodomosController, type: :controller do
     expect(response.status).to eq 200
   end
 
+  it "should respond to index" do
+    Todomo.create content: "hi", title: "heyho"
+    get :index
+    todomos = JSON.parse(response.body)
+    expect(todomos[0]['title']).to eq "heyho"
+  end
+
+  after :each do
+    Todomo.destroy_all
+  end
+
 end
