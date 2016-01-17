65c1dcaf4cf0a9f12a6533aff3fab546d8169f97
marcel <roeslerm@wh12.tu-dresden.de>
2016-01-17 14:12:59 +0100

    merge

--- spec/models/todomo_spec.rb
+++ spec/models/todomo_spec.rb
@@ -7,15 +7,15 @@ RSpec.describe Todomo, type: :model do
   end
 
   it "should have content" do
-    expect(@todomo).to respond_to(:content)
+    expect(@todomo).to respond_to :content
   end
 
   it "should have title" do
-    expect(@todomo).to respond_to(:title)
+    expect(@todomo).to respond_to :title
   end
 
   it "should have done" do
-    expect(@todomo).to respond_to(:done)
+    expect(@todomo).to respond_to :done
   end
 
   it "should have done_time" do
